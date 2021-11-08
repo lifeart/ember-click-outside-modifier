a276372bab55e88e3bc6122f00fa28cb26796078
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-11-09 01:48:56 +0300

    fix ember-try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,20 +5,21 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
+    useYarn: true,
     scenarios: [
-      {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0',
-          },
-        },
-      },
       {
         name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
             'ember-source': '~3.20.5',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.24.3',
           },
         },
       },

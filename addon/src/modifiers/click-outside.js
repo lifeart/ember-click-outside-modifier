0cc163e4daf69bf7f55f4a649e8a7f5b3b3ebe93
Sergey Astapov <SergeAstapov@gmail.com>
2022-04-06 16:52:42 -0400

    Prepare for ember-modifier v4

--- addon/src/modifiers/click-outside.js
+++ addon/src/modifiers/click-outside.js
@@ -20,40 +20,43 @@ function getEventNames({ event, events }) {
   return EVENTS;
 }
 
-export default modifier(function clickOutside(
-  element,
-  [handlerValue, useCapture] = [undefined, false],
-  hashParams = {}
-) {
-  const refEvent = new Event('clickReference');
-  const events = getEventNames(hashParams);
-  const isFunction = typeof handlerValue === 'function';
-  if (!isFunction) {
-    throw new Error('{{click-outside}}: Handler value must be a function.');
-  }
-  const handlers = [];
-  events.forEach((eventName) => {
-    const handler = (event) => {
-      if (refEvent.timeStamp > event.timeStamp) {
-        return;
-      }
-      const isClickOutside =
-        event.target !== element && !element.contains(event.target);
-      if (!isClickOutside) {
-        return;
-      }
-      handlerValue(event);
-    };
-    handlers.push([eventName, handler]);
-    document.documentElement.addEventListener(eventName, handler, useCapture);
-  });
-  return () => {
-    handlers.forEach(([eventName, handler]) => {
-      document.documentElement.removeEventListener(
-        eventName,
-        handler,
-        useCapture
-      );
+export default modifier(
+  function clickOutside(
+    element,
+    [handlerValue, useCapture] = [undefined, false],
+    hashParams = {}
+  ) {
+    const refEvent = new Event('clickReference');
+    const events = getEventNames(hashParams);
+    const isFunction = typeof handlerValue === 'function';
+    if (!isFunction) {
+      throw new Error('{{click-outside}}: Handler value must be a function.');
+    }
+    const handlers = [];
+    events.forEach((eventName) => {
+      const handler = (event) => {
+        if (refEvent.timeStamp > event.timeStamp) {
+          return;
+        }
+        const isClickOutside =
+          event.target !== element && !element.contains(event.target);
+        if (!isClickOutside) {
+          return;
+        }
+        handlerValue(event);
+      };
+      handlers.push([eventName, handler]);
+      document.documentElement.addEventListener(eventName, handler, useCapture);
     });
-  };
-});
+    return () => {
+      handlers.forEach(([eventName, handler]) => {
+        document.documentElement.removeEventListener(
+          eventName,
+          handler,
+          useCapture
+        );
+      });
+    };
+  },
+  { eager: false }
+);
